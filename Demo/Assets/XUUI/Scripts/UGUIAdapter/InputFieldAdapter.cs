e60abc9eec6b8db5165fc16e6b456c95d8561bf0
YOUNGJON <angyangjiong@outlook.com>
2019-05-08 00:59:12 +0800

    image load sprite

--- Demo/Assets/XUUI/Scripts/UGUIAdapter/InputFieldAdapter.cs
+++ Demo/Assets/XUUI/Scripts/UGUIAdapter/InputFieldAdapter.cs
@@ -23,10 +23,7 @@ namespace XUUI.UGUIAdapter
         {
             Target.onValueChanged.AddListener((val) =>
             {
-                if (OnValueChange != null)
-                {
-                    OnValueChange(val);
-                }
+                OnValueChange?.Invoke(val);
             });
         }
     }
--- Demo/Assets/XUUI/Scripts/UGUIAdapter/XUUIHelper.cs
+++ Demo/Assets/XUUI/Scripts/UGUIAdapter/XUUIHelper.cs
@@ -8,9 +8,11 @@ namespace XUUI.UGUIAdapter
     {
         public static void SetSprite(this Image image, Texture2D texture2D)
         {
+            if (texture2D == null) return;
             if (image.sprite != null)
             {
                 Object.Destroy(image.sprite);
+                image.sprite = null;
             }
             var sprite = Sprite.Create(texture2D, new Rect(0f, 0f, texture2D.width, texture2D.height), Vector2.zero);
             image.sprite = sprite;
